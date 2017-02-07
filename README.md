7960fdefb6c7f0debec3f81bf2a264b28bb23c2e
David Watson <david@bashton.com>
2017-02-07 13:16:41 +0000

    Move AWS to provider package, and add GCE provider.

--- README.md
+++ README.md
@@ -1,33 +1,52 @@
-varnish-purge-proxy
-===================
+# varnish-purge-proxy
 
 [![Build Status](https://travis-ci.org/BashtonLtd/varnish-purge-proxy.svg?branch=master)](https://travis-ci.org/BashtonLtd/varnish-purge-proxy)
 
 Proxy purge requests to multiple varnish servers
 
-Specify tags to limit instances that receive the purge request, multiple tags can be used. You must specify at least one tag.
+Works with AWS or GCE.
 
-`./varnish-purge-proxy Service:varnish Environment:live`
+## Global options
 
 You can also specify host and port to listen on:
 
-`./varnish-purge-proxy --listen=127.0.0.1 --port=8000`
+`./varnish-purge-proxy aws --listen=127.0.0.1 --port=8000`
 
 You can also specify the destination port to target:
 
-`./varnish-purge-proxy --destport=6081`
+`./varnish-purge-proxy aws --destport=6081`
 
 varnish-purge-proxy will cache the IP lookup for 60 seconds, you can change this as follows:
 
-`./varnish-purge-proxy --cache=120`
+`./varnish-purge-proxy aws --cache=120`
 
-Authentication
---------------
+## AWS
+
+Specify tags to limit instances that receive the purge request, multiple tags can be used. You must specify at least one tag.
+
+### Example
+
+`./varnish-purge-proxy aws Service:varnish Environment:live`
+
+
+### Authentication
 
 AWS access key and secret key can be added as environment variables, using either `AWS_ACCESS_KEY_ID` or `AWS_SECRET_ACCESS_KEY`.  If these are not available then IAM credentials for the instance will be checked.
 
-Building
---------
+
+## GCE
+
+Specify an instance name prefix to limit instances that receive the purge request with the `--nameprefix` argument.
+
+### Example
+
+`varnish-purge-proxy gce --credentials=creds.json --region=us-central1 --project=my-project --nameprefix=varnish`
+
+### Authentication
+
+GCE credentials should be provided using the `--credentials` argument.
+
+## Building
 
 Build a binary by running:
 
--- varnish-purge-proxy.go
+++ varnish-purge-proxy.go
@@ -26,38 +26,44 @@ import (
 	"log/syslog"
 	"net/http"
 	"net/url"
-	"strings"
+	"os"
 	"sync"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/BashtonLtd/varnish-purge-proxy/providers"
 	"gopkg.in/alecthomas/kingpin.v1"
 )
 
 var (
-	port            = kingpin.Flag("port", "Port to listen on.").Default("8000").Int()
-	cache           = kingpin.Flag("cache", "Time in seconds to cache instance IP lookup.").Default("60").Int()
-	destport        = kingpin.Flag("destport", "The destination port of the varnish server to target.").Default("80").Int()
-	listen          = kingpin.Flag("listen", "Host address to listen on, defaults to 127.0.0.1").Default("127.0.0.1").String()
-	tags            = kingpin.Arg("tag", "Key:value pair of tags to match EC2 instances.").Strings()
-	debug           = kingpin.Flag("debug", "Log additional debug messages.").Bool()
-	region          string
+	// Global application args
+	app      = kingpin.New("varnish-purge-proxy", "Proxy purge requests to multiple varnish servers.")
+	cache    = app.Flag("cache", "Time in seconds to cache instance IP lookup.").Default("60").Int()
+	debug    = app.Flag("debug", "Log additional debug messages.").Bool()
+	destport = app.Flag("destport", "The destination port of the varnish server to target.").Default("80").Int()
+	listen   = app.Flag("listen", "Host address to listen on, defaults to 127.0.0.1").Default("127.0.0.1").String()
+	port     = app.Flag("port", "Port to listen on.").Default("8000").Int()
+
+	// AWS service args
+	awsService = app.Command("aws", "Use AWS service.")
+	tags       = awsService.Arg("tag", "Key:value pair of tags to match EC2 instances.").Required().Strings()
+
+	// GCE service args
+	gceService  = app.Command("gce", "Use GCE service.")
+	credentials = gceService.Flag("credentials", "Path to service account JSON credentials").Required().String()
+	nameprefix  = gceService.Flag("nameprefix", "Instance name prefix, eg. varnish").Default("varnish").String()
+	project     = gceService.Flag("project", "Google project to discover varnish servers").Required().String()
+	region      = gceService.Flag("region", "Google region to discover varnish servers").Required().String()
+
+	// Application variables
 	resetAfter      time.Time
+	service         providers.Service
 	taggedInstances = []string{}
 )
 
-type config struct {
-	ConnectTimeout   time.Duration
-	ReadWriteTimeout time.Duration
-}
-
 func main() {
-	kingpin.Version("2.1.0")
-	kingpin.Parse()
+	kingpin.Version("3.0.0")
 
+	// Log to syslog
 	sl, err := syslog.New(syslog.LOG_NOTICE|syslog.LOG_LOCAL0, "[varnish-purge-proxy]")
 	defer sl.Close()
 	if err != nil {
@@ -67,25 +73,37 @@ func main() {
 		log.SetOutput(sl)
 	}
 
-	if len(*tags) == 0 {
-		fmt.Println("No tags specified")
-		return
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	// Register user
+	case awsService.FullCommand():
+		service = &providers.AWSProvider{
+			Tags:  *tags,
+			Debug: *debug,
+		}
+		err := service.Auth()
+		if err != nil {
+			log.Fatalln("Failed to Authenticate AWS Service:", err)
+		}
+	case gceService.FullCommand():
+		service = &providers.GCEProvider{
+			Credentials: *credentials,
+			Debug:       *debug,
+			NamePrefix:  *nameprefix,
+			Project:     *project,
+			Region:      *region,
+		}
+		err := service.Auth()
+		if err != nil {
+			log.Fatalln("Failed to Authenticate GCE Service:", err)
+		}
 	}
 
-	region, err = ec2metadata.New(session.New()).Region()
-	if err != nil {
-		log.Printf("Unable to retrieve the region from the EC2 instance %v\n", err)
-	}
-
-	// Set up access to ec2
-	svc := ec2.New(session.New(), &aws.Config{Region: &region})
-
-	go serveHTTP(*port, *listen, svc)
+	go serveHTTP(*port, *listen, service)
 
 	select {}
 }
 
-func serveHTTP(port int, host string, ec2region *ec2.EC2) {
+func serveHTTP(port int, host string, service providers.Service) {
 	timeout := time.Duration(5 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
@@ -93,7 +111,7 @@ func serveHTTP(port int, host string, ec2region *ec2.EC2) {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		requestHandler(w, r, &client, ec2region)
+		requestHandler(w, r, &client, service)
 	})
 
 	addr := fmt.Sprintf("%v:%d", host, port)
@@ -110,7 +128,7 @@ func serveHTTP(port int, host string, ec2region *ec2.EC2) {
 	log.Println(err.Error())
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request, client *http.Client, ec2region *ec2.EC2) {
+func requestHandler(w http.ResponseWriter, r *http.Request, client *http.Client, service providers.Service) {
 	// check that request is PURGE and has X-Purge-Regex header set
 	if _, exists := r.Header["X-Purge-Regex"]; !exists || r.Method != "PURGE" {
 		if *debug {
@@ -123,7 +141,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request, client *http.Client,
 	privateIPs := taggedInstances
 	// Check instance cache
 	if time.Now().After(resetAfter) {
-		privateIPs = getPrivateIPs(ec2region)
+		privateIPs = service.GetPrivateIPs()
 		resetAfter = time.Now().Add(time.Duration(*cache*1000) * time.Millisecond)
 		taggedInstances = privateIPs
 	}
@@ -178,51 +196,6 @@ func copyRequest(src *http.Request) (*http.Request, error) {
 		req.Host = host
 	}
 	return req, nil
-}
-
-func getPrivateIPs(ec2region *ec2.EC2) []string {
-	instances := []string{}
-	filters, err := buildFilter(*tags)
-	if err != nil {
-		log.Println(err)
-	}
-
-	request := ec2.DescribeInstancesInput{Filters: filters}
-	result, err := ec2region.DescribeInstances(&request)
-	if err != nil {
-		log.Println(err)
-	}
-
-	for _, reservation := range result.Reservations {
-		for _, instance := range reservation.Instances {
-			if instance.PrivateIpAddress != nil {
-				if *debug {
-					log.Printf("Adding %s to IP list\n", *instance.PrivateIpAddress)
-				}
-				instances = append(instances, *instance.PrivateIpAddress)
-			}
-		}
-	}
-
-	return instances
-}
-
-func buildFilter(tags []string) ([]*ec2.Filter, error) {
-	filters := []*ec2.Filter{}
-
-	for _, tag := range tags {
-		parts := strings.SplitN(tag, ":", 2)
-		if len(parts) != 2 {
-			return nil, fmt.Errorf("expected TAG:VALUE got %s", tag)
-		}
-		tagName := fmt.Sprintf("tag:%s", *aws.String(parts[0]))
-		filters = append(filters, &ec2.Filter{
-			Name:   &tagName,
-			Values: []*string{aws.String(parts[1])},
-		})
-	}
-	return filters, nil
-
 }
 
 func forwardRequest(r *http.Request, ip string, destport int, client http.Client, requesturl string, responseChannel chan int, wg *sync.WaitGroup) {
