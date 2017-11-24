14bb5985e8c808c317f177aec5431afb7246f80b
Zachary Stevens <zts@elastera.com>
2017-11-24 22:25:57 +0000

    Add test for copyRequest function

--- varnish-purge-proxy_test.go
+++ varnish-purge-proxy_test.go
@@ -77,3 +77,12 @@ func TestForwardRequest(t *testing.T) {
 	}
 
 }
+
+func TestCopyRequest(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://example.com/", nil)
+	req.Header.Set("X-Foo", "")
+
+	dup, _ := copyRequest(req)
+	expect(t, "host is copied", dup.Host, req.Host)
+	expect(t, "header is copied", dup.Header.Get("X-Foo"), req.Header.Get("X-Foo"))
+}
